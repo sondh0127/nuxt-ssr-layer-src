e330701190a4e99dabbde7d07deb8f776315c840
Son Hong Do <sondh0127@gmail.com>
2024-03-10 01:04:37 +0700

    css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,3 @@
-import { createResolver } from '@nuxt/kit'
-
-const { resolve } = createResolver(import.meta.url)
-
 export default defineNuxtConfig({
-  css: [
-    resolve('./hyrd-ui/hyrd-ui.css'),
-  ],
+
 })
